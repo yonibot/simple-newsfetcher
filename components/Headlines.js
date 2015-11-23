6c0fc446a6f8d281e260b0e8c32f790cf717acba
Yoni Weisbrod <yoni.weisbrod@gmail.com>
2015-11-24 01:47:58 +0200

    add date and filter action

--- components/Headlines.js
+++ components/Headlines.js
@@ -4,10 +4,11 @@ class Headlines extends Component {
   render() {
     const { items, filterText } = this.props;
     let itemList = items.map((item, i) => {
-      if (item.webTitle.indexOf(filterText) === -1) {
+      if (item.webTitle.toLowerCase()
+            .indexOf(filterText.toLowerCase()) === -1) {
         return;
       } else {
-        return (<li key={i}><a href={item.webUrl}>{item.webTitle}</a></li>);
+        return (<li key={i}><a href={item.webUrl}>{item.webTitle}</a> ({new Date(Date.parse(item.webPublicationDate)).toUTCString()})</li>);
       };
     });
     return (
